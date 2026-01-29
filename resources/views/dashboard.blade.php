358ddee1e8ec97435b6f958a031794e821c4a373
younesbahmoun <younes.bahmoun.apk@gmail.com>
2026-01-29 10:36:24 +0100

    refactor(ui): convert dashboard and profile php file to blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    @php $currentPage = 'dashboard'; @endphp
     @include('components.navbar')
 
     <!-- Main Content -->
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    @php $currentPage = 'profile'; @endphp
+    <!-- @php $currentPage = 'profile'; @endphp -->
     @include('components.navbar')
 
     <!-- Profile Section -->
