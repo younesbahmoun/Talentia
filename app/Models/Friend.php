2a0d3608e82c603af33335b1f2cefa4aca4f31ad
younesbahmoun <younes.bahmoun.apk@gmail.com>
2026-02-03 18:59:13 +0100

    feat(friend) : add belongsTo with table user

--- app/Models/Friend.php
+++ app/Models/Friend.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Friend extends Model
 {
-    //
+    /** Table name (migration created 'friend' not 'friends') */
+    protected $table = 'friend';
+
+    protected $fillable = [
+        'user_id',
+        'friend_id',
+        'status',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
