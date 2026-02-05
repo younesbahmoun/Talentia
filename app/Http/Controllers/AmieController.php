f01dc8d93426c4d112b6bfa319c706e497eb3a8c
younesbahmoun <younes.bahmoun.apk@gmail.com>
2026-02-05 14:52:20 +0100

    feat(AmieController): add function getAllAnvitation get info user from table user and profile

--- app/Http/Controllers/AmieController.php
+++ app/Http/Controllers/AmieController.php
@@ -4,9 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Friend;
+use App\Models\Profile;
+use Illuminate\Support\Facades\Auth;
 
 
-class AmieController extends Controller
-{
-
+class AmieController extends Controller {
+    public function getAllAnvitation() {
+        $friends = Friend::with('friend.profile')
+            ->where('user_id', Auth::id())
+            ->where('status', 'pending')
+            ->get();
+        return view('utilisateur.amie', compact('friends'));
+    }
 }
