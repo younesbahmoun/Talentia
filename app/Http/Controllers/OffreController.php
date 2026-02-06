9b84776690f4edeaa00824439f96dd26103183c2
younesbahmoun <younes.bahmoun.apk@gmail.com>
2026-02-06 12:14:56 +0100

    feat(OffreController): add function store

--- app/Http/Controllers/OffreController.php
+++ app/Http/Controllers/OffreController.php
@@ -11,4 +11,25 @@ class OffreController extends Controller
         $offres = Offre::all();
         return view('offres.index', compact('offres'));
     }
+
+    public function create()
+    {
+        return view('offres.create');
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'titre' => 'required|string|max:255',
+            'description' => 'required|string',
+            'image' => 'required|string|max:255',
+            'entreprise' => 'required|string|max:255',
+            'type_contrat' => 'required|in:CDI,CDD,Freelance,Stage,Alternance',
+            'status' => 'required|in:ferme,ouvert',
+            'user_id' => 'required|exists:users,id',
+        ]);
+
+        Offre::create($request->all());
+
+        return redirect()->route('offres.index')->with('success', 'Offre créée avec succès!');
+    }
 }
