3e317cc592f4701f0d3c7d848c85f83dcbc546f1
younesbahmoun <younes.bahmoun.apk@gmail.com>
2026-02-06 12:02:23 +0100

    feat(offre): add column in table offres

--- database/migrations/2026_02_06_104227_create_offres_table.php
+++ database/migrations/2026_02_06_104227_create_offres_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('offres', function (Blueprint $table) {
             $table->id();
+            $table->string('titre');
+            $table->text('description');      
+            $table->string('image')->nullable();
+            $table->string('entreprise');
+            $table->enum('type_contrat', ['CDI', 'CDD', 'Freelance', 'Stage', 'Alternance']); 
+            $table->enum('status', ['ferme', 'ouvert']);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
