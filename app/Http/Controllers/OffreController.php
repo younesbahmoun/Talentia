8d4cd5a2385548f1c384dfae6ff4801e8511b0e9
younesbahmoun <younes.bahmoun.apk@gmail.com>
2026-02-06 12:12:55 +0100

    feat(OffreController): add function index to get all  offres

--- app/Http/Controllers/OffreController.php
+++ app/Http/Controllers/OffreController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class OffreController extends Controller
 {
-
+    public function index()
+    {
+        $offres = Offre::all();
+        return view('offres.index', compact('offres'));
+    }
 }
