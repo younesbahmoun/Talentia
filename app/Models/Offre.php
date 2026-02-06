9f83aef6dce47cf8884e4ef4d4e2470a355b654f
younesbahmoun <younes.bahmoun.apk@gmail.com>
2026-02-06 12:07:40 +0100

    feat(offre): add

--- app/Models/Offre.php
+++ app/Models/Offre.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Offre extends Model
 {
-    //
+    protected $fillable = [
+        'titre',
+        'description',
+        'image',
+        'entreprise',
+        'type_contrat',
+        'status',
+        'user_id',
+    ];
 }
