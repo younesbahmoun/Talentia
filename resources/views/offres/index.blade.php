afcacdea55355a4ca904dc73f777dc46aa516d18
younesbahmoun <younes.bahmoun.apk@gmail.com>
2026-02-08 14:27:15 +0100

    feat(offres-view): add template to show all offres

---
+++ resources/views/offres/index.blade.php
@@ -0,0 +1,48 @@
+<x-master title="Offres d'emploi">
+    <div class="container my-5">
+        <div class="row g-4">
+            
+            <!-- Offre 1 -->
+            
+            <div class="col-md-6 col-lg-4">
+                <div class="card h-100">
+                    <!-- <img src="https://intranet.youcode.ma/storage/users/profile/thumbnail/1873-1760996499.png" class="card-img-top" alt="Image offre"> -->
+                    <div class="card-body">
+                        <h5 class="card-title">Titre de l'offre</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Nom de l'entreprise</h6>
+                        <span class="badge bg-primary mb-3">CDI</span>
+                        <p class="card-text">Description de l'offre d'emploi...</p>
+                        <a href="#" class="btn btn-primary">Voir détails</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Offre 2 -->
+            <div class="col-md-6 col-lg-4">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h5 class="card-title">Développeur Web</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Tech Solutions</h6>
+                        <span class="badge bg-success mb-3">CDD</span>
+                        <p class="card-text">Nous recherchons un développeur web passionné...</p>
+                        <a href="#" class="btn btn-primary">Voir détails</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Offre 3 -->
+            <div class="col-md-6 col-lg-4">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h5 class="card-title">Designer UI/UX</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Creative Agency</h6>
+                        <span class="badge bg-warning mb-3">Freelance</span>
+                        <p class="card-text">Rejoignez notre équipe créative...</p>
+                        <a href="#" class="btn btn-primary">Voir détails</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</x-master>
