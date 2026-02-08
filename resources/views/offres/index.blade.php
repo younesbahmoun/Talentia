ab4f5a1956b5f554adfa7369acf710cb39e38ce1
younesbahmoun <younes.bahmoun.apk@gmail.com>
2026-02-08 14:44:13 +0100

    feat(offres-view): add empty state and truncate description

--- resources/views/offres/index.blade.php
+++ resources/views/offres/index.blade.php
@@ -2,20 +2,24 @@
     <div class="container my-5">
         <div class="row g-4">
             
-            @foreach($offres as $offre)
-                <div class="col-md-6 col-lg-4">
-                    <div class="card h-100">
-                        <!-- <img src="https://intranet.youcode.ma/storage/users/profile/thumbnail/1873-1760996499.png" class="card-img-top" alt="Image offre"> -->
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $offre->titre }}</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">{{ $offre->entreprise }}</h6>
-                            <span class="badge bg-primary mb-3">{{ $offre->type_contrat }}</span>
-                            <p class="card-text">{{ Str::limit($offre->description, 30) }}</p>
-                            <a href="#" class="btn btn-primary">Voir détails</a>
+            @if($offres->count() > 0)
+                @foreach($offres as $offre)
+                    <div class="col-md-6 col-lg-4">
+                        <div class="card h-100">
+                            <!-- <img src="https://intranet.youcode.ma/storage/users/profile/thumbnail/1873-1760996499.png" class="card-img-top" alt="Image offre"> -->
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $offre->titre }}</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">{{ $offre->entreprise }}</h6>
+                                <span class="badge bg-primary mb-3">{{ $offre->type_contrat }}</span>
+                                <p class="card-text">{{ Str::limit($offre->description, 30) }}</p>
+                                <a href="#" class="btn btn-primary">Voir détails</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            @else
+                <p>Aucun Offres</p>
+            @endif
         </div>
     </div>
 </x-master>
