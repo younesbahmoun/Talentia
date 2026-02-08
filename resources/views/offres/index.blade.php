cc8760804aa05c056d304095490d1cbf53898b2c
younesbahmoun <younes.bahmoun.apk@gmail.com>
2026-02-08 14:31:10 +0100

    feat(offres-view): show all offres

--- resources/views/offres/index.blade.php
+++ resources/views/offres/index.blade.php
@@ -2,47 +2,20 @@
     <div class="container my-5">
         <div class="row g-4">
             
-            <!-- Offre 1 -->
-            
-            <div class="col-md-6 col-lg-4">
-                <div class="card h-100">
-                    <!-- <img src="https://intranet.youcode.ma/storage/users/profile/thumbnail/1873-1760996499.png" class="card-img-top" alt="Image offre"> -->
-                    <div class="card-body">
-                        <h5 class="card-title">Titre de l'offre</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Nom de l'entreprise</h6>
-                        <span class="badge bg-primary mb-3">CDI</span>
-                        <p class="card-text">Description de l'offre d'emploi...</p>
-                        <a href="#" class="btn btn-primary">Voir détails</a>
+            @foreach($offres as $offre)
+                <div class="col-md-6 col-lg-4">
+                    <div class="card h-100">
+                        <!-- <img src="https://intranet.youcode.ma/storage/users/profile/thumbnail/1873-1760996499.png" class="card-img-top" alt="Image offre"> -->
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $offre->titre }}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{{ $offre->entreprise }}</h6>
+                            <span class="badge bg-primary mb-3">{{ $offre->type_contrat }}</span>
+                            <p class="card-text">{{ Str::limit($offre->description, 30) }}</p>
+                            <a href="#" class="btn btn-primary">Voir détails</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-
-            <!-- Offre 2 -->
-            <div class="col-md-6 col-lg-4">
-                <div class="card h-100">
-                    <div class="card-body">
-                        <h5 class="card-title">Développeur Web</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Tech Solutions</h6>
-                        <span class="badge bg-success mb-3">CDD</span>
-                        <p class="card-text">Nous recherchons un développeur web passionné...</p>
-                        <a href="#" class="btn btn-primary">Voir détails</a>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Offre 3 -->
-            <div class="col-md-6 col-lg-4">
-                <div class="card h-100">
-                    <div class="card-body">
-                        <h5 class="card-title">Designer UI/UX</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Creative Agency</h6>
-                        <span class="badge bg-warning mb-3">Freelance</span>
-                        <p class="card-text">Rejoignez notre équipe créative...</p>
-                        <a href="#" class="btn btn-primary">Voir détails</a>
-                    </div>
-                </div>
-            </div>
-
+            @endforeach
         </div>
     </div>
 </x-master>
