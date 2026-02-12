d421522aa6b9c699c3fe09287004faa6668b8e2a
abderrahmane140 <bsarabderrahmane01@gmail.com>
2026-02-12 15:15:35 +0100

    add the model for the message

---
+++ app/Models/Message.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'sender_id',
+        'receiver_id',
+        'content',
+    ];
+
+    // Sender relation
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    // Receiver relation
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+}
